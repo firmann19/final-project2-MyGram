9547c9465354b64215f1f59ea62619784f90699f
firmann19 <firman19@gmail.com>
2022-11-24 22:13:14 +0700

    new lagi

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const PORT = process.env.PGPORT;
 app.get('/', (req,res) => {
   res.status(200).json({message:"final project 2 tim 4"})
 });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
