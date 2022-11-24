95bb7c94482840b463ed4c9d383dadfbff03375c
firmann19 <firman19@gmail.com>
2022-11-24 22:12:26 +0700

    baru lagi

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const socialMediaRoute = require("./routers/socialMediaRoute");
 const commentRoute = require("./routers/commentRoute");
 const PORT = process.env.PGPORT;
 
+app.get('/', (req,res) => {
+  res.status(200).json({message:"final project 2 tim 4"})
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
